Modify EllipsoidFit to make XY grid fixed; so now the surface 'pushes' the grid. Add a script that transforms coordinates after a rigid rotation in roll, pitch, and yaw.

_test/surface_mv_demo.py
@@ -10,18 +10,6 @@ from surface import EllipsoidFit
 
 np.set_printoptions(precision=2)
 
-
-class ThreadedAction(Thread):
-    def __init__(self, ellipsoid: EllipsoidFit, **kwargs):
-        Thread.__init__(self, **kwargs)
-        self._e = ellipsoid
-
-    def run(self):
-        print("Fitting ellipsoid to data ...")
-        self._e.optimize_parameters()
-        print('done.')
-
-
 if __name__ == "__main__":
     mlab.figure(1, bgcolor=(0, 0, 0), size=(500, 500))
     mlab.clf()
@@ -43,12 +31,12 @@ if __name__ == "__main__":
     u0, u1, u2, theta = 1, 0, 0, np.pi / 2
     r = R.from_quat([u0, u1, u2, theta / 2])
 
-    e = EllipsoidFit(source.parametric_function, xyz_0=(x0, y0, z0), sample_spacing=10)
-    e.volume = np.empty(shape=(25, 512, 512))
-    e.u0, e.u1, e.u2, e.theta = u0, u1, u2, theta
+    e = EllipsoidFit(source.parametric_function, xyz_0=(x0, y0, z0), sample_spacing=50)
+    e.u0, e.u1, e.u2, e.theta = u1, -u2, -u0, theta
+    e.volume = np.empty(shape=(25, a * 4, b * 2))
 
     points = mlab.points3d(e.xl, e.yl, e.zl, [1] * len(e.xl), color=(1, 0, 1), scale_factor=10)
-    o = mlab.quiver3d(0, 1, 0, scale_factor=2 * b)
+    o = mlab.quiver3d(u0, u1, u2, scale_factor=2 * b)
 
     o.actor.actor.orientation = r.as_euler('XYZ', degrees=True)
 
@@ -58,27 +46,29 @@ if __name__ == "__main__":
     actor.mapper.scalar_visibility = False  # don't colour ellipses by their scalar indices into colour map
     actor.property.backface_culling = True  # gets rid of weird rendering artifact when opacity is < 1
     actor.property.specular = 0.1
-    actor.actor.orientation = r.as_euler('YZX', degrees=True)
+    # actor.actor.orientation = r.as_euler('YZX', degrees=True)
     actor.actor.origin = np.array([0, 0, 0])
     actor.actor.scale = np.array([1, 1, 1])
-    actor.actor.position = np.array([0, 0, 0])
+    actor.actor.position = np.array([x0, y0, z0])
     actor.property.representation = ['wireframe', 'surface'][1]
 
 
-    @mlab.animate(delay=200)
+    @mlab.animate(delay=500)
     def update_visualisation(srf, pts, o):
-        theta = np.linspace(0, 2 * np.pi, 360)
+        theta = np.linspace(0, 2 * np.pi, 10)
         xr = np.linspace(-1000, 1000, 40)
         i = j = 0
         while True:
             i = (i + 1) % len(theta)
             j = (j + 1) % len(xr)
-            x0, x1, x2 = xr[j], 0, 0
+            x0, y0, z0 = xr[j], xr[j], 0
             r = R.from_quat([u0, u1, u2, np.cos(theta[i] / 2)])
 
             # order of angles follows [pitch, roll, yaw]
             srf.actor.actor.orientation = r.as_euler('YZX', degrees=True)
             o.actor.actor.orientation = r.as_euler('YZX', degrees=True)
+            srf.actor.actor.position = np.array([x0, y0, z0])
+            o.actor.actor.position = np.array([x0, y0, z0])
 
             # --------------------------------------------------------------
             #  DEBUG info
@@ -101,8 +91,8 @@ if __name__ == "__main__":
             print(f"logical check if q1==q2 {np.dot(r.as_quat(canonical=False), r2.as_quat(canonical=False))}")
 
             # recompute points on surface given new coordinates
-            e.x0, e.y0, e.z0 = x0, x1, x2
-            e.u0, e.u1, e.u2 = u1, -u2, u0
+            e.x0, e.y0, e.z0 = x0, y0, z0
+            e.u0, e.u1, e.u2 = u1, -u2, -u0
             e.theta = theta[i]
             e.eval_surf()
 

surface/_ellipsoid_fit.py
@@ -38,7 +38,7 @@ class EllipsoidFit:
         self._u1 = 0
         self._u2 = 0
         self._th = 0
-        self._r = None
+        self._r: R = None
 
         self._surf_eval = False
 
@@ -67,9 +67,19 @@ class EllipsoidFit:
         }
 
     def z(self, x, y):
-        z2 = 1 - (x - self._x0) ** 2 / self._a2 - (y - self._y0) ** 2 / self._b2
-        z = self._c * np.sqrt(z2)
-        # return [self._z0 - z, self._z0 + z]
+        # beta, alpha, gamma = self._r.as_euler('YZX', degrees=False)
+        # cxy = x * np.sin(beta) - y * np.sin(gamma) * np.cos(beta)
+        # cz = np.cos(beta) * np.cos(gamma)
+
+        R = self._r.as_matrix()
+        xyz = R.dot(np.array([x - self._x0, y - self._y0, 0]))
+
+        cxy = -x * R[2, 0] + y * R[2, 1]
+        cz = R[2, 2]
+
+        z2 = 1 - xyz[0] ** 2 / self._a2 - xyz[1] ** 2 / self._b2
+        z = self._c * (np.sqrt(z2) - cxy) / cz
+
         return self._z0 - z
 
     def __setattr__(self, name, value):
@@ -95,48 +105,24 @@ class EllipsoidFit:
         self._dtype = vol.dtype
 
         self.projected_img_2d = np.zeros(shape=(self._w, self._h), dtype=self._dtype)
-        self._calc_z_surf()
-
-    def _calc_z_surf(self):
-        pts = []
-        x0 = self._w / self._spac / 2
-        y0 = self._h / self._spac / 2
-        for xi, yi in itertools.product(range(int(self._w / self._spac)), range(int(self._h / self._spac))):
-            xi -= x0
-            yi -= y0
-            xi *= self._spac
-            yi *= self._spac
-            pts.append([xi, yi, self.z(xi, yi)])
-        self._pts = np.asarray(pts).T
-        with self.calculating_semaphore:
-            self.pts = self._pts.copy()
-            self.xl, self.yl, self.zl = self.pts
+        self.eval_surf()
 
     def eval_surf(self):
         if self._surf_eval:
             return
 
-        self._calc_z_surf()
-
         u0, u1, u2, cth_2 = self._u0, self._u1, self._u2, np.cos(self._th / 2)
-        # print(f"eval_surf {R.from_quat([u0, u1, u2, cth_2]).as_euler('YZX', degrees=True)}")
-        # print(f"eval_surf {R.from_quat([u0, u2, u1, cth_2]).as_euler('YZX', degrees=True)}")
-        # print(f"eval_surf {R.from_quat([u1, u0, u2, cth_2]).as_euler('YZX', degrees=True)}")
-        # print(f"eval_surf {R.from_quat([u1, u2, u0, cth_2]).as_euler('YZX', degrees=True)}")
-        # print(f"eval_surf {R.from_quat([u2, u0, u1, cth_2]).as_euler('YZX', degrees=True)}")
-        # print(f"eval_surf {R.from_quat([u2, u1, u0, cth_2]).as_euler('YZX', degrees=True)}")
+        self._r = R.from_quat([u0, u1, u2, cth_2])
 
-        r = R.from_quat([u0, u1, u2, cth_2])
-        # r = R.from_quat([u0, u2, u1, cth_2])
-        # r = R.from_quat([u1, u0, u2, cth_2])
-        # r = R.from_quat([u1, u2, u0, cth_2])
-        # r = R.from_quat([u2, u0, u1, cth_2])
-        # r = R.from_quat([u2, u1, u0, cth_2])
-        X = np.array([[self._x0, self._y0, self._z0]]).T
+        pts = []
+        for xi, yi in itertools.product(range(int(self._w / self._spac)), range(int(self._h / self._spac))):
+            xi *= self._spac
+            yi *= self._spac
+            pts.append([xi, yi, self.z(xi, yi)])
+        self._pts = np.asarray(pts).T
         with self.calculating_semaphore:
-            # np.copyto(self.pts, (r.as_matrix().dot(self.pts - X) + X))
-            np.copyto(self.pts, (r.as_matrix().dot(self._pts - X)))
-            # np.copyto(self.pts, r.apply(self.pts.T).T)
+            self.pts = self._pts.copy().astype(int)
+            self.xl, self.yl, self.zl = self.pts
 
         self._surf_eval = True
 
@@ -150,7 +136,8 @@ class EllipsoidFit:
         zf = np.floor(zf[zix]).astype(int)
         changes = len(zf)
         if changes > 0:
-            for xi, yi, zi in zip(np.array(self.xl)[zix], np.array(self.yl)[zix], zf):
+            for xi, yi, zi in zip(np.array(self.xl / self._spac).astype(int)[zix],
+                                  np.array(self.yl / self._spac).astype(int)[zix], zf):
                 # if 0 <= xi < self._w and 0 <= yi < self._h and 0 <= zi < self._nz:
                 self.projected_img_2d[xi, yi] = self._vol[zi, xi, yi]
                 changes += 1